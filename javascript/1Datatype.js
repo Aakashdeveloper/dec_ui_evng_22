107749bd4668da73b55c9656f364489be4c02fe6
aakashdeveloper <ahanda205@gmail.com>
2022-01-19 21:58:04 +0530

    datatype

---
+++ javascript/1Datatype.js
@@ -0,0 +1,111 @@
+//es5> Ecma Script5
+//es6> Ecma Script6
+
+String > 'hiii' "hey" `hello`
+Number > 6 6565 67576565 78.76 .767656
+Boolean > true false
+
+//es5
+var a = "hi"
+var b = 10
+var c = true
+
+a
+'hi'
+typeof(a)
+'string'
+typeof(b)
+'number'
+typeof(c)
+'boolean'
+var d = "20"
+undefined
+d
+'20'
+typeof(d)
+'string'
+var d = 10.78
+undefined
+typeof(d)
+'number'
+var d = .78
+undefined
+typeof(d)
+'number'
+var d = "true"
+undefined
+typeof(d)
+'string'
+
+var a = 10
+var b = 20
+a+b(addition)
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+a%b
+10
+b%a
+0
+
+2%4 2
+3%4 3
+4%4 0
+5%2
+1
+5%3
+2
+
+var a = "hi"
+var b = "javascript"
+
+a+b(concat)
+'hijavascript'
+a-b
+NaN(not a number)
+a*b
+NaN
+a/b
+NaN
+
+string+string = string
+string+number = string
+number+string = string
+number+number = number
+
+"10"+20+"30"
+"1020"+"30"
+'102030'
+
+10+20+"30"
+30+"30"
+'3030'
+
+"10"+20+30
+"1020"+30
+"102030"
+
+"10"+20+"30"-1
+102029
+10+20+"30"-1
+3029
+"10"+20+30-1
+102029
+
+"hi"-1
+NaN
+"10"-1
+9
+"10"+1
+'101'
+2*"5"
+10
+"10"/"2"
+5
+"10a"/"2"
+NaN
+2*"5a"
---
+++ javascript/2Datatype.js
@@ -0,0 +1,63 @@
+var a = true
+var b = true
+true is equal to 1
+false is equal to 0
+
+a+b
+2
+true+false
+1
+10+true
+11
+10/false
+Infinity
+
+"hi"+true
+'hitrue'
+"true"-"true"
+NaN
+"true"+"true"
+'truetrue'
+
+
+var a = "10"
+var b = "2"
+
+a+b
+'102'
+parseInt(a)
+10
+parseInt(b)
+2
+parseInt(a)+parseInt(b)
+12
+var a = "hi"
+parseInt(a)
+NaN
+var a = "10.22"
+parseInt(a)+parseInt(b)
+12
+parseFloat(a)+parseFloat(b)
+12.22
+
+//es6
+Number(a)+Number(a)
+20.44
+
+var a = "10delhi"
+var b = "delhi10"
+var c = "10delhi20"
+
+parseInt(a)
+10
+parseInt(b)
+NaN
+parseInt(c)
+10
+
+Number(a)
+NaN
+Number(b)
+NaN
+Number(c)
+NaN
